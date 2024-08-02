c77b6c9eadead0ba06703ccc298c98b1e6ee18e6
Ashwin-Developer <ashwinraj2253353@gmail.com>
2024-08-03 01:13:10 +0530

    almost sorted the issue

--- Assets/Script/CharacterMovement.cs
+++ Assets/Script/CharacterMovement.cs
@@ -5,21 +5,41 @@ using UnityEngine;
 public class CharacterMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
-
-    private CharacterController characterController;
+    private Rigidbody rb;
+    private GravityManager _gravityState;
 
     void Start()
     {
-        characterController = GetComponent<CharacterController>();
+        rb = GetComponent<Rigidbody>();
+        _gravityState = GravityManager.instance;
     }
 
     void Update()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
+        MovePlayer();
+    }
 
-        Vector3 move = transform.right * moveHorizontal + transform.forward * moveVertical;
+    void MovePlayer()
+    {
+        
+        if(_gravityState.CurrentState == GravityState.Up || _gravityState.CurrentState == GravityState.Down)
+        {
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
 
-        characterController.Move(move * moveSpeed * Time.deltaTime);
+            Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical) * moveSpeed;
+            rb.velocity = new Vector3(movement.x, rb.velocity.y, movement.z);
+        }
+
+        else if(_gravityState.CurrentState == GravityState.Left || _gravityState.CurrentState == GravityState.Right)
+        {
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
+
+            Vector3 movement = new Vector3(0, moveHorizontal, moveVertical) * moveSpeed;
+            rb.velocity = new Vector3(rb.velocity.x, movement.y, movement.z);
+        }
+
+        
     }
 }
--- Assets/Script/GravityManager.cs
+++ Assets/Script/GravityManager.cs
@@ -2,8 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum GravityState
+{
+    Up,
+    Down, 
+    Left, 
+    Right
+}
 public class GravityManager : MonoBehaviour
 {
+    public static GravityManager instance;
+    public GravityState CurrentState;
+
     private static readonly Vector3 _gravityUpVector = new Vector3(0, 9.8f, 0);
     private static readonly Vector3 _gravityDownVector = new Vector3(0, -9.8f, 0);
     private static readonly Vector3 _gravityLeftVector = new Vector3(-9.8f, 0, 0);
@@ -11,38 +21,46 @@ public class GravityManager : MonoBehaviour
 
     [SerializeField] private Transform _player;
 
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
     void Update()
     {
         if (Input.GetButtonDown("Gravity Left"))
         {
-            ChangeGravity(_gravityLeftVector, "Left arrow clicked");
-            Vector3 rotation =  new Vector3(0, 0, -90);
-            _player.rotation = Quaternion.Euler(rotation);
+            ChangeGravity(_gravityLeftVector, "Left arrow clicked", new Vector3(0, 0, -90), GravityState.Left);
+
         }
         else if (Input.GetButtonDown("Gravity Right"))
         {
-            ChangeGravity(_gravityRightVector, "Right arrow clicked");
-            Vector3 rotation = new Vector3(0, 0, 90);
-            _player.rotation = Quaternion.Euler(rotation);
+            ChangeGravity(_gravityRightVector, "Right arrow clicked", new Vector3(0, 0, 90), GravityState.Right);
+
         }
         else if (Input.GetButtonDown("Gravity Up"))
         {
-            ChangeGravity(_gravityUpVector, "Up arrow clicked");
-            Vector3 rotation = new Vector3(180, 0, 0);
-            _player.rotation = Quaternion.Euler(rotation);
+            ChangeGravity(_gravityUpVector, "Up arrow clicked", new Vector3(180, 0, 0), GravityState.Up);
         }
         else if (Input.GetButtonDown("Gravity Down"))
         {
-            ChangeGravity(_gravityDownVector, "Down arrow clicked");
-            Vector3 rotation = new Vector3(0, 0, 0);
-            _player.rotation = Quaternion.Euler(rotation);
+            ChangeGravity(_gravityDownVector, "Down arrow clicked", new Vector3(0, 0, 0), GravityState.Down);
         }
     }
 
-    private void ChangeGravity(Vector3 gravityVector, string message)
+    private void ChangeGravity(Vector3 gravityVector, string message, Vector3 position, GravityState state)
     {
         Physics.gravity = gravityVector;
         Debug.Log(message);
+        _player.rotation = Quaternion.Euler(position);
+        CurrentState = state; 
     }
 
 }
