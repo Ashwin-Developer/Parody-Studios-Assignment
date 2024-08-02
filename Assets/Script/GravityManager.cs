247dbb145e64eabb58cdf5dd9f5d291bbe77f84a
Ashwin-Developer <88386942+Ashwin-Developer@users.noreply.github.com>
2024-08-02 20:46:40 +0530

    Gravity Manager done

--- Assets/Script/GravityManager.cs
+++ Assets/Script/GravityManager.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class GravityManager : MonoBehaviour
 {
-    
 
-    // Update is called once per frame
+    private static readonly Vector3 _gravityUpVector = new Vector3(0, 9.8f, 0);
+    private static readonly Vector3 _gravityDownVector = new Vector3(0, -9.8f, 0);
+    private static readonly Vector3 _gravityLeftVector = new Vector3(9.8f, 0, 0);
+    private static readonly Vector3 _gravityRightVector = new Vector3(-9.8f, 0, 0);
+
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButtonDown("Gravity Left"))
         {
-            Debug.Log("Button Clicked");
-            Physics.gravity = new Vector3(-10f, 0, 0);
+            ChangeGravity(_gravityLeftVector, "Left arrow clicked");
+        }
+        else if (Input.GetButtonDown("Gravity Right"))
+        {
+            ChangeGravity(_gravityRightVector, "Right arrow clicked");
+        }
+        else if (Input.GetButtonDown("Gravity Up"))
+        {
+            ChangeGravity(_gravityUpVector, "Up arrow clicked");
+        }
+        else if (Input.GetButtonDown("Gravity Down"))
+        {
+            ChangeGravity(_gravityDownVector, "Down arrow clicked");
         }
     }
+
+    private void ChangeGravity(Vector3 gravityVector, string message)
+    {
+        Physics.gravity = gravityVector;
+        Debug.Log(message);
+    }
+
 }
