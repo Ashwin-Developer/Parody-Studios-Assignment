68f63c06227b9de04c67074d229325c20439e3f5
Ashwin-Developer <88386942+Ashwin-Developer@users.noreply.github.com>
2024-08-03 17:44:15 +0530

    Jump mechanics done

--- Assets/Script/CharacterMovement.cs
+++ Assets/Script/CharacterMovement.cs
@@ -5,11 +5,17 @@ using UnityEngine;
 public class CharacterMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
+    public float jumpForce = 7f; // Add this for jump force
+    public Transform cameraTransform;  // Reference to the camera transform
+    public LayerMask groundLayer; // Layer to define what is considered ground
+
     private Rigidbody rb;
     private GravityManager _gravityState;
     private Animator _animator;
-    public Transform cameraTransform;  // Reference to the camera transform
+    private bool isGrounded;
 
+    [SerializeField] private GameObject _groundCheck;
+    [SerializeField] private float groundCheckRadius = 0.1f;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -20,6 +26,7 @@ public class CharacterMovement : MonoBehaviour
     void Update()
     {
         MovePlayer();
+        HandleJump();
 
         Debug.Log(rb.velocity.sqrMagnitude);
         // Animation
@@ -42,13 +49,11 @@ public class CharacterMovement : MonoBehaviour
         Vector3 forward = cameraTransform.forward;
         Vector3 right = cameraTransform.right;
 
-        
         forward.Normalize();
         right.Normalize();
 
         Vector3 desiredMoveDirectionXandZ = (forward * moveVertical + right * moveHorizontal).normalized * moveSpeed;
         Vector3 desiredMoveDirectionZandY = (forward * moveHorizontal + right * moveVertical).normalized * moveSpeed;
-
 
         switch (_gravityState.CurrentState)
         {
@@ -69,5 +74,40 @@ public class CharacterMovement : MonoBehaviour
                 rb.velocity = new Vector3(rb.velocity.x, movement.y, movement.z);
                 break;
         }
+    }
+
+    void HandleJump()
+    {
+        // Check if the player is on the ground
+        isGrounded = Physics.CheckSphere(_groundCheck.transform.position, groundCheckRadius, groundLayer);
+
+        if (isGrounded && Input.GetButtonDown("Jump"))
+        {
+            _animator.Play("Jump");
+            switch (_gravityState.CurrentState)
+            {
+                case GravityState.Down:
+                    rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+                    break;
+
+                case GravityState.Up:
+                    rb.AddForce(Vector3.down * jumpForce, ForceMode.Impulse);
+                    break;
+
+                case GravityState.Left:
+                    rb.AddForce(Vector3.right * jumpForce, ForceMode.Impulse);
+                    break;
+
+                case GravityState.Right:
+                    rb.AddForce(Vector3.left * jumpForce, ForceMode.Impulse);
+                    break;
+            }
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(_groundCheck.transform.position, groundCheckRadius);
     }
 }
